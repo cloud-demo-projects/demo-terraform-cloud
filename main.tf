302fb2ef54177fb446f2a7f03fecf34d1b6c975a
Nipun Bahri <bahri.nipun@gmail.com>
2022-01-22 17:50:18 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "k8s" {
+    name     = "test-tf-cloud"
+    location = "westeurope"
+}
